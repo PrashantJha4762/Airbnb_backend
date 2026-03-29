04aa87e6ae2696967a3182cb3be18b42b4c4e8a8
Prashant <jhaprashantkumar73@gmail.com>
2026-03-29 19:07:25 +0530

    .

--- AuthInGo/router/router.go
+++ AuthInGo/router/router.go
@@ -1,9 +1,14 @@
 package router
 
-import "github.com/go-chi/chi/v5"
+import (
+	"AuthInGo/controllers"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func SetUpRouter() *chi.Mux {
 	router:=chi.NewRouter();
+	router.Get("/ping",controllers.PingHandler)
 
 	return router;
 }
